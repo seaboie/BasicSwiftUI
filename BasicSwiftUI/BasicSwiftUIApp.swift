eaadb899d65e4adff49ad0e0fcc7a196d1edf2f6
Kritbovorn <ktaweeyossak@yahoo.com>
2021-12-23 11:49:01 +0700

    Initial Commit

---
+++ BasicSwiftUI/BasicSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicSwiftUIApp.swift
+//  BasicSwiftUI
+//
+//  Created by Kritbovorn Taweeyossak on 23/12/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct BasicSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
