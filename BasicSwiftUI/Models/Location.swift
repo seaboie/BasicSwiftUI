427dcdea675a1357bd93c5a1e6c08a2a3ea64865
Kritbovorn <ktaweeyossak@yahoo.com>
2021-12-28 18:04:20 +0700

    finish Lesson 2

---
+++ BasicSwiftUI/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  BasicSwiftUI
+//
+//  Created by Kritbovorn Taweeyossak on 28/12/2564 BE.
+//
+
+import Foundation
+import CoreLocation
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let linK: String
+}
